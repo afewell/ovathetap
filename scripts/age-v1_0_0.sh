ef480b89a81f60c38d4771aea761f377259423db
afewell <afewell@gmail.com>
2022-12-19 10:19:33 +0000

    fix: secrets handling solution to resolve #29

--- scripts/age-v1_0_0.sh
+++ scripts/age-v1_0_0.sh
@@ -11,3 +11,12 @@ mv age/age-keygen /usr/local/bin
 rm -rf age.tar.gz
 rm -rf age
 cd /${home_dir}
+## age usage example
+# # base64 encode file before encrypting
+# cat raw_source_file.abc | base64 > source_file.abc
+# # encrypt source_file.abc - user will be prompted for passphrase
+# age -e -p -o encrypted_source_file.abc.age source_file.abc
+# # decrypt source_file.abc - user will be prompted for passphrase
+# age -d -o decrypted_source_file.abc encrypted_source_file.abc.age
+# # base64 decode after decrypting
+# cat decrypted_source_file.abc | base64 -d > raw_source_file.abc
