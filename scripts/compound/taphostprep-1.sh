eb02ca15056e979a667991a8920fcdf0bbec57e1
afewell <afewell@gmail.com>
2022-12-22 23:36:02 +0000

    fix: refactor taphostprep-1 to remove extensive redundancy to resolve #53 and resolve #54

--- scripts/compound/taphostprep-1.sh
+++ scripts/compound/taphostprep-1.sh
@@ -5,10 +5,11 @@
 ## Script variables
 ### Inject envars from input file
 hostusername="${hostusername:-viadmin}"
+script_tmp_dir=${script_tmp_dir:-tmp/taphostprep}
 echo "hostusername: $(echo $hostusername)"
 echo 'source "/home/${hostusername}/ovathetap/scripts/inputs/vars.env.sh"'
 source "/home/${hostusername}/ovathetap/scripts/inputs/vars.env.sh"
-echo "Here is the env:"
+echo "Here is the env command output:"
 env
 echo "The above line is end of the env output"
 ### Injected Vars that are used in this file:
@@ -16,6 +17,32 @@ echo "The above line is end of the env output"
 ### Inject Secret variables from input file 
 # commented because no secrets needed in this file
 # source "/home/${hostusername}/ovathetap/scripts/inputs/secrets.env.sh"
+
+# Check if -u flag is passed as first argument
+if [ "$1" = "-u" ]; then
+  install_all=y
+fi
+
+# Define an array of packages to install
+packages=(
+  "openssh-server"
+  "ntp"
+  "curl"
+  "vim"
+  "git"
+  "jq"
+)
+
+# Define an array of scripts to run
+scripts=(
+  "age-v1_0_0.sh"
+  "dockerce.sh"
+  "vscode.sh"
+  "minikube.sh"
+  "kubectl.sh"
+  "helm.sh"
+  "dnsutils.sh"
+)
 
 ## Define Functions
 # func_msg_block echos a provided message in a block of hashtags and tees it to the install log
@@ -40,9 +67,9 @@ func_msg_block () {
             message="${1}"
             ;;
     esac
-    echo "##################################################" | tee -a /tmp/taphostprep.log
-    echo "# ${message}" | tee -a /tmp/taphostprep.log
-    echo "##################################################" | tee -a /tmp/taphostprep.log
+    echo "##################################################" | tee -a /${script_tmp_dir}/taphostprep.log
+    echo "# ${message}" | tee -a /${script_tmp_dir}/taphostprep.log
+    echo "##################################################" | tee -a /${script_tmp_dir}/taphostprep.log
 }
 
 # func_apt_install updates apt and then executes apt install ${1} -y
@@ -51,10 +78,12 @@ func_msg_block () {
 # Stub: func_apt_install "${1}"
 function func_apt_install () {
     apt_package="${1}"
-    func_msg_block "pre" "${apt_package}"
-    apt-get update | tee -a /tmp/taphostprep.log
-    apt install "${apt_package}" -y | tee -a /tmp/taphostprep.log
-    func_msg_block "post" "${apt_package}"
+    if [ "$install_all" ] || [ "$install" = "y" ] || [ "$install" = "Y" ]; then
+        func_msg_block "pre" "${apt_package}"
+        apt-get update | tee -a /${script_tmp_dir}/taphostprep.log
+        apt install "${apt_package}" -y | tee -a /${script_tmp_dir}/taphostprep.log
+        func_msg_block "post" "${apt_package}"
+    fi
 }
 
 # func_install_script downloads an install script from ${script_dir_url}/${1} and sources it for execution from this env
@@ -62,117 +91,103 @@ function func_apt_install () {
 # Stub: func_install_script "<script filename>"
 function func_install_script () {
     script_filename=${1}
-    func_msg_block "pre" "${script_filename}"
-    source "/${ovathetap_scripts}/${script_filename}" | tee -a /tmp/taphostprep.log
-    post_install_msg="# Finished Installing: ${script_filename} "
-    func_msg_block "post" "${script_filename}"
+    if [ "$install_all" ] || [ "$install" = "y" ] || [ "$install" = "Y" ]; then
+        func_msg_block "pre" "${script_filename}"
+        source "/${ovathetap_scripts}/${script_filename}" | tee -a /${script_tmp_dir}/taphostprep.log
+        func_msg_block "post" "${script_filename}"
+    fi
 }
 
 # Main
-mkdir "/${script_tmp_dir}"
+if [ ! -d /${script_tmp_dir} ]
+then
+    mkdir "/${script_tmp_dir}"
+    chown "${hostusername}":"${hostusername}" "/${script_tmp_dir}"
+fi
+
+# Refresh snap to prevent sporadic Ubuntu error
+killall snap-store
+snap refresh
+
 # Setup passwordless sudo
-echo "${hostusername} ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/${hostusername}"
-
-# The following line check to see if this script was called with the -u flag, which sets the {install_all} variable to y, which provides an unattended/noninteractive execution
-if [ "${1}" = "-u" ]; then install_all=y; fi
-
-if [ "${install_all}"  ]; then install=y; else read -p  "Refresh snap to prevent sporadic Ubuntu error? (y/n):" install; fi
-if [ "$install" = "y" ] || [ "$install" = "Y" ]
-then
-    killall snap-store
-    snap refresh
+if [ "$install_all" ]; then
+    install=y
+else
+    read -p "Configure host for passwordless sudo? (y/n):" install
+fi
+if [ "$install" = "y" ] || [ "$install" = "Y" ]; then
+    echo "${hostusername} ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/${hostusername}"
 fi
 
-if [ "${install_all}"  ]; then install=y; else read -p  "Install Open SSH Server to Allow Remote SSH Connections to this host? (y/n):" install; fi
-if [ "$install" = "y" ] || [ "$install" = "Y" ]
-then
-    func_apt_install "openssh-server"
-fi
 
-if [ "${install_all}"  ]; then install=y; else read -p  "Install ntp? (y/n):" install; fi
-if [ "$install" = "y" ] || [ "$install" = "Y" ]
-then
-    func_apt_install "ntp"
-fi
+# Install packages
+for package in "${packages[@]}"; do
+  if [ "$install_all" ]; then
+    install=y
+  else
+    read -p "Install $package? (y/n):" install
+  fi
+  func_apt_install $package
+done
 
-if [ "${install_all}"  ]; then install=y; else read -p  "Install curl? (y/n):" install; fi
-if [ "$install" = "y" ] || [ "$install" = "Y" ]
-then
-    func_apt_install "curl"
-fi
-
-if [ "${install_all}"  ]; then install=y; else read -p  "Install vim? (y/n):" install; fi
-if [ "$install" = "y" ] || [ "$install" = "Y" ]
-then
-    func_apt_install "vim"
-fi
-
-if [ "${install_all}"  ]; then install=y; else read -p  "Install git? (y/n):" install; fi
-if [ "$install" = "y" ] || [ "$install" = "Y" ]
-then
-    func_apt_install "git"
-fi
-
-if [ "${install_all}"  ]; then install=y; else read -p  "Install age? (y/n):" install; fi
-if [ "$install" = "y" ] || [ "$install" = "Y" ]
-then
-    func_install_script "age-v1_0_0.sh"
-fi
-
-if [ "${install_all}"  ]; then install=y; else read -p  "Install Docker CE? (y/n):" install; fi
-if [ "$install" = "y" ] || [ "$install" = "Y" ]
-then
-    func_install_script "dockerce.sh"
-fi
-
-if [ "${install_all}"  ]; then install=y; else read -p  "Install VS Code? (y/n):" install; fi
-if [ "$install" = "y" ] || [ "$install" = "Y" ]
-then
-    func_install_script "vscode.sh"
-fi
-
-if [ "${install_all}"  ]; then install=y; else read -p  "Install JQ? (y/n):" install; fi
-if [ "$install" = "y" ] || [ "$install" = "Y" ]
-then
-    func_apt_install "jq"
-fi
-
-if [ "${install_all}"  ]; then install=y; else read -p  "Install minikube? (y/n):" install; fi
-if [ "$install" = "y" ] || [ "$install" = "Y" ]
-then
-    func_install_script "minikube.sh"
-fi
-
-if [ "${install_all}"  ]; then install=y; else read -p  "Install kubectl? (y/n):" install; fi
-if [ "$install" = "y" ] || [ "$install" = "Y" ]
-then
-    func_install_script "kubectl.sh"
-fi
-
-if [ "${install_all}"  ]; then install=y; else read -p  "Install helm? (y/n):" install; fi
-if [ "$install" = "y" ] || [ "$install" = "Y" ]
-then
-    func_install_script "helm.sh"
-fi
-
-if [ "${install_all}"  ]; then install=y; else read -p  "Install dnsmasq? (y/n):" install; fi
-if [ "$install" = "y" ] || [ "$install" = "Y" ]
-then
-    func_install_script "dnsmasq.sh"
-fi
-
-if [ "${install_all}"  ]; then install=y; else read -p  "Setup private certificate authority? (y/n):" install; fi
-if [ "$install" = "y" ] || [ "$install" = "Y" ]
-then
-    func_install_script "certificateauthority.sh"
-fi
+# Run scripts
+for script in "${scripts[@]}"; do
+  if [ "$install_all" ]; then
+    install=y
+  else
+    read -p "Run script $script? (y/n):" install
+  fi
+  func_install_script $script
+done
 
 
 
 ## The below command should be the last thing that executes
 
-if [ -f /tmp/postactions.txt ]
+if [ -f /${script_tmp_dir}/postactions.txt ]
 then
-    cat /tmp/postactions.txt
-    rm /tmp/postactions.txt
+    cat /${script_tmp_dir}/postactions.txt
+    rm /${script_tmp_dir}/postactions.txt
 fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Refresh snap to prevent sporadic Ubuntu error
+if [ "$install_all" ] || [ "$install" = "y" ] || [ "$install" = "Y" ]; then
+  echo "Refreshing snap..."
+  killall snap-store
+  snap refresh
+fi
+
+# Install packages
+for package in "${packages[@]}"; do
+  if [ "$install_all" ]; then
+    install=y
+  else
+    read -p "Install $package? (y/n):" install
+  fi
+  func_apt_install $package
+done
+
+# Run scripts
+for script in "${scripts[@]}"; do
+  if [ "$install_all" ]; then
+    install=y
+  else
+    read -p "Run script $script? (y/n):" install
+  fi
+  func_install_script $script
+done
